3bf0e341d73064054ca80f39e3add5f684628233
AlvaroCespedes <aecespedes@miuandes.cl>
2020-04-06 16:32:28 -0400

    Inicio de compras.

---
+++ Lab 3/Compra.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_3
+{
+    class Compra : Producto
+    {
+        List<string> carro;
+        public Compra()
+        {
+            carro = new List<string>();
+        }
+        public void AgregarCarro(string id)
+        {
+            carro.Add(id);
+        }
+    }
+}
